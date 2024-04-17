b5b305f937d999ffcec83ece081b0a099e7f7c9d
bebek <harisnop@gmail.com>
2024-04-17 22:10:34 +0700

    testing scope variabel count
    melakukan simulasi jika inisiasi didalam fungsi increment dan di luar fungsi

--- passenger-counter/app.js
+++ passenger-counter/app.js
@@ -6,11 +6,14 @@
 // myDogAge = myAge * humanDogRatio;
 // console.log(myDogAge);
 
-let counter = document.getElementById("counter-element");
+let counter = document.getElementById('counter-element');
 
-let count = 0;
+let count = 0; //inisasi secara global. 
 function increment(){
     count+=1;
     counter.innerText=count;
+}
 
+function save(){
+    console.log(count) // dapat akses count hasil increment
 }
