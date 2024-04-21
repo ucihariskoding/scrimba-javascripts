9f357d62ed7d1b775e5131cc3ced6880fd13fa1d
bebek <harisnop@gmail.com>
2024-04-21 15:17:07 +0700

    fitur save dan previous save

--- passenger-counter/app.js
+++ passenger-counter/app.js
@@ -1,19 +1,14 @@
-// document.getElementById('counter').innerText
-// let count = 0;
-// console.log(count);
-// let myAge = 99;
-// humanDogRatio = 7;
-// myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge);
-
+let saveblock = document.getElementById('save-block')
 let counter = document.getElementById('counter-element');
-
 let count = 0; //inisasi secara global. 
+
 function increment(){
     count+=1;
     counter.innerText=count;
 }
 
 function save(){
+    let countStr = count + '-'
+    saveblock.innerText += countStr
     console.log(count) // dapat akses count hasil increment
 }
---
+++ passenger-counter/welcome-message/app.js
@@ -0,0 +1,8 @@
+let welcom = document.getElementById('welcome-el')
+console.log(welcom)
+let nama = "Soeharto"
+let greeting = "welcome beks "
+
+
+welcom.innerText = greeting + nama ;
+welcom.innerText += "💰"
